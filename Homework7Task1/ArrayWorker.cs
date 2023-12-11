fe4d06c77fe3621e9c8ef1f1aad209b23291bbc6
Vasili Savin <vasili.savin@dcbi.aero>
2023-12-11 21:38:38 +0300

    Homework 7

---
+++ Homework7Task1/ArrayWorker.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework7
+{
+    public class ArrayWorker
+    {
+        public static T[] InvertArray<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int oppositeIndex = array.Length - 1 - i;
+                (array[oppositeIndex], array[i]) = (array[i], array[oppositeIndex]);
+            }
+            return array;
+        }
+    }
+}
