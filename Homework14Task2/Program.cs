8b9aab636bb7f297673091867e7fb70cdf7b9e3d
Vasili Savin <vasili.savin@dcbi.aero>
2024-01-21 12:54:14 +0300

    Homework14

---
+++ Homework14Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Homework14Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int a = ReadFromConsole();
+            Console.WriteLine("enter second number");
+            int b = ReadFromConsole();
+            if (b == 0)
+            {
+                throw new ArgumentException("divider must be not equal zero");
+            }
+
+            Console.WriteLine($"division result: {a / (decimal)b}");
+        }
+
+        static int ReadFromConsole()
+        {
+            while (true)
+            {
+                try
+                {
+                    int number =  int.Parse(Console.ReadLine());
+                    if (number < 0 || number > 255)
+                    {
+                        throw new ArgumentException("number should be between 0 and 255");
+                    }
+                    return number;
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine($"{e.Message}. Try again");
+                }
+            }
+            
+        }
+
+        static void CheckRange(int x)
+        {
+           
+        }
+    }
+}
