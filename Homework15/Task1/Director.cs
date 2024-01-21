9de1ece75fcdbe0948d8cee4dcdf12c1f8b52039
Vasili Savin <vasili.savin@dcbi.aero>
2024-01-21 16:21:37 +0300

    Homework15

---
+++ Homework15/Task1/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15.Task1
+{
+    internal class Director
+    {
+        public event EventHandler<StudentIsAddedArgs> StudentIsAdded;
+        public void OnStudentAdded(StudentIsAddedArgs args)
+        {
+            StudentIsAdded?.Invoke(this, args);         
+        }
+    }
+}
---
+++ Homework15/Task1/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15.Task1
+{
+    internal class Student
+    {   
+        public String Name { get; set; }
+        public String Surname { get; set; }
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+
+    }
+}
