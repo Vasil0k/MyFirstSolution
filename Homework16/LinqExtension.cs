0861ba50e76c9806cfd2a9203625fccafdea1aa8
Vasili Savin <vasili.savin@dcbi.aero>
2024-01-22 21:37:58 +0300

    Homework16

---
+++ Homework16/LinqExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework16
+{
+    public static class LinqExtension
+    {
+        public static T Median<T>(this IEnumerable<T> collection)
+        {
+            if (!collection.Any())
+            {
+                throw new ArgumentException("collection must not be empty");
+            }
+
+            return collection.ElementAt(collection.Count() / 2);
+        }
+    }
+}
