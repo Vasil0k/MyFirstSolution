0c8d484ad9574dc76a7b5ea9ceb1d53180092b34
Vasili Savin <vasili.savin@dcbi.aero>
2024-01-29 20:18:14 +0300

    Homework18

---
+++ Homework18Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework18Task1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Task 1
+            Thread thread1 = new Thread(() =>
+            {
+                char[] chars = Enumerable.Range('a', 'z' - 'a' + 1).Select(ch => (char) ch).ToArray();
+                foreach (char ch in chars)
+                {
+                    Console.WriteLine(ch);
+                    Thread.Sleep(100);
+                }
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(100);
+                }
+            });
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
---
+++ Homework18Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework18Task2
+{
+    internal class Program
+    {
+        private static int counter = 0;
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            List<Thread> threads =  Enumerable.Range(1, 100).Select(i => new Thread(Action) { Name = i.ToString()}).ToList();
+            threads.ForEach(t => t.Start());
+            threads.ForEach(t => t.Join());
+            Console.WriteLine("Joined");
+            Console.WriteLine("Expected: 2000000");
+            Console.WriteLine($"Actual: {counter}");
+        }
+
+        static void Action()
+        {
+            for (int i = 0; i < 20000; i++)
+            {
+                mutex.WaitOne();
+                counter++;                
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
